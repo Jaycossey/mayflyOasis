4ff92ce1c174700ea5b544f6acd3608083166fc3
Arion Mehmeti <60946518+arionitas@users.noreply.github.com>
2024-01-17 03:39:47 +0000

    Loading screen animation is working and hiding the Navbar before it completes its cycle

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import LoadingScreen from './components/LoadingScreen'
+import Navbar from './components/Navbar'
+import './App.css'
+
+
+function App(){
+  const [loading, setLoading] = useState(true);
+
+  const handleLoadingFinished = () => {
+    setLoading(false)
+  }
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={[<LoadingScreen key="loading" onFinished={handleLoadingFinished} />, <Navbar key="navbar" />]} />
+        {/* other components */}
+      </Routes>
+    </Router>
+  )
+}
+
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Logo from '../assets/logos/mfo-full-logo-nobg.svg' 
+import PropTypes from 'prop-types';
+
+const Navbar = ({ loading}) =>(
+  loading ? null:
+  <nav className="fixed z-10 flex items-center justify-between w-full h-20 border-2 border-black">
+    <div className="flex items-center justify-center w-full max-w-lg mx-auto overflow-hidden lg:w-auto lg:mx-0">
+      <img src={Logo} alt="MFO Logo" className="h-20 w-30 m30" />
+      <span className="hidden text-xl font-semibold tracking-tight lg:block"></span>
+    </div>
+    <div className="flex items-center justify-center w-full max-w-xs mx-auto lg:w-auto lg:mx-0 lg:justify-end">
+      <Link to="/" className="px-6 py-2 text-black rounded lg:mx-2 lg:my-0">Home</Link>
+      <Link to="/about" className="px-6 py-2 text-black rounded lg:mx-2 lg:my-0">About</Link>
+      <Link to="/contact" className="px-6 py-2 text-black rounded lg:mx-2 lg:my-0">Contact</Link>
+    </div>
+  </nav>
+)
+
+Navbar.propTypes = {
+  loading: PropTypes.bool.isRequired
+}
+export default Navbar;

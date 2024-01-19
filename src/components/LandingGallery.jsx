e465c9fc8ac51166c4f453fd1794e67b5c43efb3
Annie <anam.k@live.com>
2024-01-19 18:50:38 +0000

    add data

--- src/components/LandingGallery.jsx
+++ src/components/LandingGallery.jsx
@@ -4,39 +4,40 @@ import artistArray from '../assets/MockData/ArtistArray';
 
 // Spotlight gallery for home page
 const LandingGallery = () => {
+  //
+  const artist = artistArray[0];
 
-    // 
-    const artist = artistArray[0];
-
-    return (
-        // Container for gallery
-        <div id="homeGalleryContainer" 
-            className="bg-white 
+  return (
+    // Container for gallery
+    <div
+      id='homeGalleryContainer'
+      className='bg-white 
                         p-20
                         grid
                         grid-cols-5
                         grid-rows-5
                         m-auto
                         border-2
-                        border-red-500">
-            
-            {/* Button to iterate previous artist in spotlight */}
-            <Button direction={"\<"} />
+                        border-red-500'
+    >
+      {/* Button to iterate previous artist in spotlight */}
+      <Button direction={'<'} />
 
-            {/* Container for artist spotlight information */}
-            <div id="contentContainer"
-                className="border-4
+      {/* Container for artist spotlight information */}
+      <div
+        id='contentContainer'
+        className='border-4
                             row-start-1
                             row-span-5
-                            col-span-3">
-                <SpotlightData artist={artist} />
-            </div>
+                            col-span-3'
+      >
+        <SpotlightData artist={artist} />
+      </div>
 
-            {/* Button to iterate next artist in spotlight */}
-            <Button direction={"\>"} />
-
-        </div>
-    );
-}
+      {/* Button to iterate next artist in spotlight */}
+      <Button direction={'>'} />
+    </div>
+  );
+};
 
 export default LandingGallery;

2fca543edb48c01fa848c8c1902dde9ab59ee8dc
jaycossey <jaycossey@live.com>
2024-01-23 14:59:21 +0000

    added team comments on fetch text function

--- src/components/utils/fetchText.jsx
+++ src/components/utils/fetchText.jsx
@@ -1,3 +1,5 @@
+// Fetch request for text content, can be reused for artist pages, current issues with displaying data
+// see about section (after run dev) for details, shows in console but not on screen
 const fetchText = (path) => {
     fetch(path)
         .then((res) => res.text())

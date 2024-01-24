12f01914f6d81a2b6008a37bff1587a440cf1d82
tigerkaplan <husniyeerparun@gmail.com>
2024-01-24 15:12:34 +0000

    error container added

--- src/components/Registration/Register.jsx
+++ src/components/Registration/Register.jsx
@@ -7,6 +7,16 @@ import {
   FaGlobe,
   FaUser
 } from "react-icons/fa";
+// Formik variables
+import { useFormik, Formik, Form } from "formik";
+import { signUpSchema } from "./schema";
+const initialValues = {
+  username: "",
+  name: "",
+  email: "",
+  password: "",
+  confirmPassword: ""
+};
 
 export default function Register() {
   // Choose from the list of countries
@@ -27,101 +37,166 @@ export default function Register() {
 
     fetchCountries();
   }, []);
+
+  //  Formik validation for registering
+  const {
+    values,
+    handleBlur,
+    handleChange,
+    handleSubmit,
+  } = useFormik({
+    initialValues,
+    validationSchema: signUpSchema,
+    onSubmit: (values, actions) => {
+      console.log(values);
+      actions.resetForm();
+    }
+  });
   return (
     <div
       className="flex flex-col items-center justify-center h-screen bg-cover"
       style={{ backgroundImage: "url(your-background-image-url.jpg)" }}
     >
-      <div className="bg-white bg-opacity-30 backdrop-filter backdrop-blur-md 
-p-8 rounded-lg shadow-md">
+      <div className="bg-white bg-opacity-30 backdrop-filter backdrop-blur-md p-8 rounded-lg shadow-md">
         <h2 className="text-3xl text-gray-800 font-semibold mb-6">
           Create an Account at Mayfly Oasis
         </h2>
-        <form className="flex flex-col items-center">
-          {/* -------- FullName --------*/}
-          <div className="flex items-center mb-4">
-            <FaUser className="mr-2" />
-            <input
-              type="text"
-              className="w-64 p-2 border-b border-gray-500 bg-transparent 
-utline-none placeholder-gray-500"
-              placeholder="Your FullName"
-            />
-          </div>
-          {/* -------- Email --------*/}
-          <div className="flex items-center mb-4">
-            <FaEnvelope className="mr-2" />
-
-            <input
-              type="email"
-              className="w-64 p-2 border-b border-gray-500 bg-transparent 
-outline-none placeholder-gray-500"
-              placeholder="Your Email"
-            />
-          </div>
-          {/* -------- DOB -------- */}
-          <div className="flex items-center mb-4">
-            <FaCalendar className="mr-2" />
-            <input
-              type="date"
-              className="w-64 p-2 border-b border-gray-500 bg-transparent 
-outline-none placeholder-gray-500"
-              placeholder="Date of Birth"
-            />
-          </div>
-          {/* -------- Country --------*/}
-          <div className="flex items-center mb-4">
-            <FaGlobe className="mr-2" />
-            <select
-              value={selectedCountry}
-              onChange={e => setSelectedCountry(e.target.value)}
-              className="w-64 p-2 border-b border-gray-500 bg-transparent 
-outline-none placeholder-gray-500"
-            >
-              <option value="" disabled>
-                Select your Country
-              </option>
-              {countries.map(country =>
-                <option key={country.name.common} value={country.name.common}>
-                  {country.name.common}
-                </option>
-              )}
-            </select>
-          </div>
-          {/* -------- Password -------- */}
-          <div className="flex items-center mb-4">
-            <FaLock className="mr-2" />
-            <input
-              type="password"
-              className="w-64 p-2 border-b border-gray-500 bg-transparent 
-outline-none placeholder-gray-500"
-              placeholder="Password"
-            />
-          </div>
-          {/* -------- ConfirmPassword -------- */}
-          <div className="flex items-center mb-4">
-            <FaLock className="mr-2" />
-            <input
-              type="password"
-              className="w-64 p-2 border-b border-gray-500 bg-transparent 
-outline-none placeholder-gray-500"
-              placeholder="Confirm Password"
-            />
-          </div>
-          {/* -------- Register Button -------- */}
-          <button
-            type="submit"
-            className="bg-black text-white rounded-md py-2 w-64 hover:bg-black-600"
-          >
-            Register
-          </button>
-          <p className="mt-4 text-gray-600">
-            Already have an account at Mayfly Oasis?{" "}
-            <a href="/login" className="text-black-500">
-              Login here
-            </a>
-          </p>
-        </form>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={signUpSchema}
+          onSubmit={handleSubmit}>
+          {/* restructure error */}
+          {({ errors, touched }) =>
+            <Form className="flex flex-col items-center">
+              {/************..................................Name.................................************/}
+              <div className="flex items-center mb-4">
+                <FaUser className="mr-2" />
+                <input
+                  type="text"
+                  className="w-64 p-2 border-b border-gray-500 bg-transparent outline-none placeholder-gray-500"
+                  placeholder="Name"
+                  name="name"
+                  value={values.name}
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                />
+              </div>
+              {/*--------------- Name Error Container ---------------  */}
+              <div className="errorContainer">
+                {errors.name &&
+                  touched.name &&
+                  <p className="formError">
+                    {errors.name}
+                  </p>}
+              </div>
+              {/************.................................Email.................................************/}
+              <div className="flex items-center mb-4">
+                <FaEnvelope className="mr-2" />
+                <input
+                  type="email"
+                  className="w-64 p-2 border-b border-gray-500 bg-transparent outline-none placeholder-gray-500"
+                  placeholder="Your Email"
+                  name="email"
+                  value={values.email}
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                />
+              </div>
+              {/*--------------- Email Error Container ---------------  */}
+              <div className="errorContainer">
+                {errors.email &&
+                  touched.email &&
+                  <p className="formError">
+                    {errors.email}
+                  </p>}
+              </div>
+              {/************..................................DOB.................................************/}
+              <div className="flex items-center mb-4">
+                <FaCalendar className="mr-2" />
+                <input
+                  type="date"
+                  className="w-64 p-2 border-b border-gray-500 bg-transparent outline-none placeholder-gray-500"
+                  placeholder="Date of Birth"
+                />
+              </div>
+              {/************..................................Country.................................************/}
+              <div className="flex items-center mb-4">
+                <FaGlobe className="mr-2" />
+                <select
+                  value={selectedCountry}
+                  onChange={e => setSelectedCountry(e.target.value)}
+                  className="w-64 p-2 border-b border-gray-500 bg-transparent outline-none placeholder-gray-500"
+                >
+                  <option value="" disabled>
+                    Select your Country
+                  </option>
+                  {countries.map(country =>
+                    <option
+                      key={country.name.common}
+                      value={country.name.common}
+                    >
+                      {country.name.common}
+                    </option>
+                  )}
+                </select>
+              </div>
+              {/************..................................Password.................................************/}
+              <div className="flex items-center mb-6">
+                <FaLock className="mr-2" />
+                <input
+                  type="password"
+                  className="w-64 p-2 border-b border-gray-500 bg-transparent outline-none placeholder-gray-500"
+                  placeholder="Your Password"
+                  name="password"
+                  value={values.password}
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                />
+              </div>
+              {/*--------------- Password Error Container ---------------  */}
+              <div className="errorContainer">
+                {errors.password &&
+                  touched.password &&
+                  <p className="formError">
+                    {errors.password}
+                  </p>}
+              </div>
+              {/************..................................Confirm Password...............................************/}
+              <div className="flex items-center mb-6">
+                <FaLock className="mr-2" />
+                <input
+                  type="password"
+                  className="w-64 p-2 border-b border-gray-500 bg-transparent outline-none placeholder-gray-500"
+                  placeholder="Confirm Password"
+                  name="confirmPassword"
+                  value={values.confirmPassword}
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                />
+              </div>
+              {/*--------------- Confirm Password Error Container ---------------  */}
+              <div className="errorContainer">
+                {errors.confirmPassword &&
+                  touched.confirmPassword &&
+                  <p className="formError">
+                    {errors.confirmPassword}
+                  </p>}
+              </div>
+              {/************.............................Register Button.............................************/}
+              <button
+                type="submit"
+                className="bg-black text-white rounded-md py-2 w-64 hover:bg-black-600"
+              >
+                Register
+              </button>
+              <p className="mt-4 text-gray-600">
+                Already have an account at Mayfly Oasis?{" "}
+                <a href="/login" className="text-black-500">
+                  Login here
+                </a>
+              </p>
+            </Form>}
+        </Formik>
       </div>
     </div>
   );

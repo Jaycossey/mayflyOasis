5faabcc1ab3db83897aadaa6887c71ee5a161672
jaycossey <jaycossey@live.com>
2024-01-25 12:22:14 +0000

    added basic functionality and randomness to next button, need to fix previous button to handle looping backwards

--- src/components/landingSection/LandingGallery.jsx
+++ src/components/landingSection/LandingGallery.jsx
@@ -1,14 +1,39 @@
+import { useState } from 'react';
 import SpotlightData from '../SpotlightData';
 import Button from './Button';
 import artistArray from '../../assets/MockData/ArtistArray';
 import PRODUCTS from '../../assets/MockData/Products';
 import rngesus from '../utils/rngesus';
+
+const previousGallery = [];
+
+
 // Spotlight gallery for home page
 const LandingGallery = () => {
-  // CREATE RNGSUS FOR PRODUCTS[i];
-  const i = rngesus(PRODUCTS.length);
-  const artist = PRODUCTS[i];
-  const prevIterations = [];
+    // state handlers for gallery rotation, random initial index
+    const [currentGallery, setCurrentGallery] = useState(rngesus(PRODUCTS.length));  
+    // // 
+    // const [previousGallery, setPreviousGallery] = useState([]);
+
+    
+    const handleClick = () => {
+      console.log("click", event.target.id);
+
+      // If there are no current prev iterations
+      if(event.target.id === "<" && previousGallery.length === 0) {
+        return;
+
+      // handle "next" artist showcase
+      } else if(event.target.id === ">") {
+        previousGallery.push(currentGallery);
+        setCurrentGallery(rngesus(PRODUCTS.length));
+
+      // handle previous. have a bug here, need to work out how to track the previous iteration
+      } else if(event.target.id === "<") {
+        setCurrentGallery(previousGallery[0]);
+      }
+    }
+
 
     return (
         // Container for gallery
@@ -22,19 +47,19 @@ const LandingGallery = () => {
                         bg-opacity-50
                         border-2">
             
-            {/* Button to iterate previous artist in spotlight */}
-            <Button direction={"<"} />
+            {/* Button to iterate previous artist in spotlight  onClick={handlePrevious} */}
+            <Button direction={"<"} onClick={handleClick} />
 
             {/* Container for artist spotlight information */}
             <div id="contentContainer"
                 className="col-span-3 
                             row-span-5 
                             row-start-1">
-                <SpotlightData artist={artist} />
+                <SpotlightData artist={PRODUCTS[currentGallery]} />
             </div>
 
-      {/* Button to iterate next artist in spotlight */}
-      <Button direction={'>'} />
+      {/* Button to iterate next artist in spotlight  onClick={setCurrentGallery(rngesus(PRODUCTS.length))} */}
+      <Button direction={'>'} onClick={handleClick} />
     </div>
   );
 };

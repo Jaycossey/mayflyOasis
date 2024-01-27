ef3438db9f390aca9962f42468abd974d0e73257
jaycossey <jaycossey@live.com>
2024-01-27 19:43:35 +0000

    updated file paths and links for about team member section, netlify not displaying profile photos correctly, added potential fix, will need confirmation

--- src/components/aboutSection/teamMemberArray.jsx
+++ src/components/aboutSection/teamMemberArray.jsx
@@ -3,29 +3,29 @@ const teamArray = [
         name: "Ian",
         occupation: "Front End Developer & Repo person",
         quote: "Automate everything",
-        image: "src/assets/images/ian.png",
+        image: "/src/assets/images/ian.png",
         link: "https://www.linkedin.com/in/ian-j-scott/",
     },
     {
         name: "Husniye",
         occupation: "Front End Developer & Visionary",
         quote: "Cats are cool, but Front End Devs are cooler!",
-        image: "src/assets/images/husniye.jpg",
-        link: "https://www.linkedin.com/in/ian-j-scott/"
+        image: "/src/assets/images/husniye.jpg",
+        link: "https://www.linkedin.com/in/husniyeerparun/"
     },
     {
         name: "Arion",
         occupation: "Front End Developer & UI Designer",
         quote: "Music is life",
-        image: "src/assets/images/arion.png",
-        link: "https://www.linkedin.com/in/ian-j-scott/"
+        image: "/src/assets/images/arion.jpg",
+        link: "https://www.linkedin.com/in/arion-mehmeti/"
     },
     {
         name: "Annie",
         occupation: "Front End Developer & Professional Nitpicker",
         quote: "The best way to not feel hopeless is to get up and do something.",
-        image: "src/assets/images/annie.jpg",
-        link: "https://www.linkedin.com/in/ian-j-scott/"
+        image: "/src/assets/images/annie.jpg",
+        link: "https://www.linkedin.com/in/annie-r-0a2a216a/"
     }
 ]
 

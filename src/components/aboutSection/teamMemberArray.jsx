17feb96fffd5fb8024397d2062dc8e1eb7bd09a8
Arion Mehmeti <60946518+arionitas@users.noreply.github.com>
2024-01-27 20:26:33 +0000

    fixed navbar responsiveness for mobile viewports. Fixed Loading Screen responsiveness

--- src/components/aboutSection/teamMemberArray.jsx
+++ src/components/aboutSection/teamMemberArray.jsx
@@ -11,21 +11,21 @@ const teamArray = [
         occupation: "Front End Developer & Visionary",
         quote: "Cats are cool, but Front End Devs are cooler!",
         image: "src/assets/images/husniye.jpg",
-        link: "https://www.linkedin.com/in/ian-j-scott/"
+        link: "https://www.linkedin.com/in/husniyeerparun/"
     },
     {
         name: "Arion",
         occupation: "Front End Developer & UI Designer",
         quote: "Music is life",
-        image: "src/assets/images/arion.png",
-        link: "https://www.linkedin.com/in/ian-j-scott/"
+        image: "src/assets/images/arion.jpg",
+        link: "https://www.linkedin.com/in/arion-mehmeti/"
     },
     {
         name: "Annie",
         occupation: "Front End Developer & Professional Nitpicker",
         quote: "The best way to not feel hopeless is to get up and do something.",
         image: "src/assets/images/annie.jpg",
-        link: "https://www.linkedin.com/in/ian-j-scott/"
+        link: "https://www.linkedin.com/in/annie-r-0a2a216a/"
     }
 ]
 
